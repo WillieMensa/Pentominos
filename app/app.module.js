c1251deae409d3f1873e0e29d53e33c9c7e0e1e7
Esger thuis <esgerj@gmail.com>
2016-11-14 22:47:17 +0100

    Draggable in separate file

--- app/app.module.js
+++ app/app.module.js
@@ -44,47 +44,4 @@ app.controller('mainController', ['$scope', 'dataservice', function($scope, data
     }
 
     console.log($scope.pentominos);
-}])
-
-.directive('draggable', function($document) {
-    return function(scope, element, attr) {
-        var startX = 0, startY = 0,
-            pentoX = 0, pentoY = 0,
-            x = 0, y = 0;
-        var container = null;
-
-        element.css({
-            position: 'relative',
-            cursor: 'pointer'
-        });
-
-        element.on('mousedown', function(event) {
-            // Prevent default dragging of selected content
-            event.preventDefault();
-            pentoX = attr.$$element.parent()[0].offsetLeft;
-            pentoY = attr.$$element.parent()[0].offsetTop;
-            startX = event.clientX - pentoX;
-            startY = event.clientY - pentoY;
-            $document.on('mousemove', mousemove);
-            $document.on('mouseup', mouseup);
-            container = attr.$$element.parent();
-            // console.log(container);
-        });
-
-        function mousemove(event) {
-            x = event.clientX - startX;
-            y = event.clientY - startY;
-            // console.log("x: " + x + " y: " + y)
-            container.css({
-                left: x + 'px',
-                top: y + 'px'
-            });
-        }
-
-        function mouseup(event) {
-            console.log(x,y);
-            $document.unbind('mousemove', mousemove);
-            $document.unbind('mouseup', mouseup);
-        }
-    }
-});
+}]);
---
+++ app/components/draggable.js
@@ -0,0 +1,48 @@
+// Draggable directive for the pentomino blocks
+app.directive('draggable', function($document) {
+    return function(scope, element, attr) {
+        var startX = 0, startY = 0,
+            pentoX = 0, pentoY = 0,
+            x = 0, y = 0;
+        var container = null;
+
+        element.css({
+            position: 'relative',
+            cursor: 'pointer'
+        });
+
+        element.on('mousedown', function(event) {
+            // Prevent default dragging of selected content
+            event.preventDefault();
+            pentoX = attr.$$element.parent()[0].offsetLeft;
+            pentoY = attr.$$element.parent()[0].offsetTop;
+            startX = event.clientX - pentoX;
+            startY = event.clientY - pentoY;
+            $document.on('mousemove', mousemove);
+            $document.on('mouseup', mouseup);
+            container = attr.$$element.parent();
+            // console.log(container);
+        });
+
+        function setCss(x,y) {
+            container.css({
+                left: x + 'px',
+                top: y + 'px'
+            });
+        }
+
+        function mousemove(event) {
+            x = event.clientX - startX;
+            y = event.clientY - startY;
+            setCss(x,y);
+        }
+
+        function mouseup(event) {
+            x = Math.round(x / scope.partSize) * scope.partSize;
+            y = Math.round(y / scope.partSize) * scope.partSize;
+            setCss(x,y);
+            $document.unbind('mousemove', mousemove);
+            $document.unbind('mouseup', mouseup);
+        }
+    }
+});
