d0dfc84f5ef2620936fe795d0da2ebf92e90d373
Esger thuis <esgerj@gmail.com>
2016-12-08 19:57:56 +0100

    rgba colors

--- assets/data/colors.js
+++ assets/data/colors.js
@@ -1,42 +1,42 @@
 $scope.colors = [
     {
         "name" : "b",
-        "color" : "midnightblue"
+        "color" : "rgba(25, 25, 112, 0.9)"
     },{
         "name" : "c",
-        "color" : "darkviolet"
+        "color" : "rgba(148, 0, 211, 0.9)"
     },{
         "name" : "f",
-        "color" : "darkorange"
+        "color" : "rgba(255, 140, 0, 0.9)"
     },{
         "name" : "i",
-        "color" : "maroon"
+        "color" : "rgba(128, 0, 0, 0.9)"
     },{
         "name" : "l",
-        "color" : "darkgreen"
+        "color" : "rgba(0, 100, 0, 0.9)"
     },{
         "name" : "n",
-        "color" : "magenta"
+        "color" : "rgba(255, 0, 255, 0.9)"
     },{
         "name" : "t",
-        "color" : "limegreen"
+        "color" : "rgba(50, 205, 50, 0.9)"
     },{
         "name" : "v",
-        "color" : "deepskyblue"
+        "color" : "rgba(0, 191, 255, 0.9)"
     },{
         "name" : "w",
-        "color" : "teal"
+        "color" : "rgba(0, 128, 128, 0.9)"
     },{
         "name" : "x",
-        "color" : "red"
+        "color" : "rgba(255, 0, 0, 0.9)"
     },{
         "name" : "y",
-        "color" : "gold"
+        "color" : "rgba(255, 215, 0, 0.9)"
     },{
         "name" : "z",
-        "color" : "mediumblue"
+        "color" : "rgba(0, 0, 205, 0.9)"
     },{
         "name" : "o",
-        "color" : "darkslategray"
+        "color" : "rgba(47, 79, 79, 0.9)"
     }
 ];
