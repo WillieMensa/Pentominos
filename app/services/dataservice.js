436baa3d403826cc0a4eb48369985c12754b047f
Esger thuis <esgerj@gmail.com>
2016-12-31 19:59:04 +0100

    hide board sizes when no solutions < 2 convert old solutions

--- app/services/dataservice.js
+++ app/services/dataservice.js
@@ -25,11 +25,40 @@ angular.module('pentominoApp')
                     return response.data;
 				});
 		},
-        getSolutions : function(boardTypes){
+        getSolutions : function(boardTypes) {
+            // Convert old solutions
+            var convert = function(solutions) {
+                    var convertedSolutions = {};
+                    for (var boardType in solutions) {
+                        if (solutions.hasOwnProperty(boardType)) {
+                            if (solutions[boardType].length) {
+                                for (var i = 0; i < solutions[boardType].length; i++) {
+                                    solutions[boardType][i] = solutions[boardType][i].split('#');
+                                    solutions[boardType][i].shift();
+                                    for (var j = 0; j < solutions[boardType][i].length; j++) {
+                                        solutions[boardType][i][j] = solutions[boardType][i][j].split('');
+                                        solutions[boardType][i][j] = solutions[boardType][i][j].join('_')
+                                    }
+                                    solutions[boardType][i] = '#' + solutions[boardType][i].join('#');
+                                }
+                            }
+                        }
+                        convertedSolutions[boardType] = solutions[boardType];
+                    }
+                    return convertedSolutions;
+                };
+
             if (localStorage.getItem("pentominos2")) {
                 solutions = JSON.parse(localStorage.getItem("pentominos2"));
             } else {
-                solutions = {};
+                if (localStorage.getItem("pentominos")) {
+                    solutions = JSON.parse(localStorage.getItem("pentominos"));
+                    solutions = convert(solutions);
+                    localStorage.setItem("pentominos2", JSON.stringify(solutions));
+                    localStorage.removeItem("pentominos");
+                } else {
+                    solutions = {};
+                }
             }
             for (var type in boardTypes) {
                 if (boardTypes.hasOwnProperty(type)) {
